287a375b5c3340f1f079ee103aa950a0733ead92
Alex Tripp <alexhtripp@gmail.com>
2024-08-31 23:36:13 -0500

    implemented size input

--- src/form/CreateBoardFormComponent.tsx
+++ src/form/CreateBoardFormComponent.tsx
@@ -14,13 +14,23 @@ function CreateBoardFormComponent({}) {
     const [boardSize, setBoardSize] = React.useState(5);
     const [useFreeSpace, setFreeSpace] = React.useState(true);
     const [errorMessage, setErrorMessage] = React.useState('');
-    function handleBoardSizeChange(value: number) {
+    function handleBoardSizeChange(value: number) : void {
         let newSize = value;
         if (useFreeSpace && value % 2 === 0) {
-            newSize++;
-            return;
+            if (value > boardSize) newSize++;
+            else newSize--;
         }
-        setBoardSize(value);
+        if (newSize < 3) {
+            newSize = 3;
+        }
+        setBoardSize(newSize);
+        setErrorMessage('');
+    }
+    function handleFreeSpaceCheckboxClick(value: boolean) : void {
+        setFreeSpace(value);
+        if (boardSize % 2 === 0) {
+            setBoardSize(boardSize + 1);
+        }
         setErrorMessage('');
     }
     return (
@@ -40,13 +50,13 @@ function CreateBoardFormComponent({}) {
                         label="Board Size"
                         type="number"
                         value={boardSize}
-                        onChange={(e) => setBoardSize(Number(e.target.value))}
+                        onChange={(e) => handleBoardSizeChange(Number(e.target.value))}
                     />
                     <Checkbox 
                         checked={useFreeSpace}
-                        onChange={(e) => setFreeSpace(e.target.checked)}
+                        onChange={(e) => handleFreeSpaceCheckboxClick(e.target.checked)}
                     />
-                    <Typography variant="caption">Include free space cells (Forces an odd-numbered board)</Typography>
+                    <Typography variant="caption">Include free space cells (Forces an odd-numbered board size)</Typography>
                 </Box>
                 <Button type="submit" variant="contained" color="primary">
                     Create Board
